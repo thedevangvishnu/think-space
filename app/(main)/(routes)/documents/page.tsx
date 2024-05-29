18cfe5c9c1b9a1cdd0ac0fb8943e503e126b4ab6
Devang Vishnu <thedevangvishnu@gmail.com>
2024-05-29 12:01:56 +0530

    Add the user item in sidebar and an image in documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div className="h-full">
-      <div className="h-full bg-neutral-900 flex items-center justify-center border-b-2 border-foreground">
-        First
-      </div>
-      <div className="h-full bg-neutral-900 flex items-center justify-center">
-        Second
-      </div>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/images/employer.png"
+        alt="Employer Icon"
+        width="300"
+        height="300"
+        className="dark:hidden"
+      />
+      <Image
+        src="/images/employer-dark.png"
+        alt="Employer Icon"
+        width="300"
+        height="300"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Think Space
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
     </div>
   );
 };
